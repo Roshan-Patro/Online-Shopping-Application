5fef5bd95992de58d43a3de380640317a927d8f9
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2023-01-15 01:10:54 +0530

    Project Tamplet Added

---
+++ OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/OnlineShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.OnlineShoppingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingApplication.class, args);
+	}
+
+}
---
+++ OnlineShoppingApplication/src/test/java/com/OnlineShoppingApp/OnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineShoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
