a9e6d9f378485b129306e9bcb580781abc321154
Shiv-96 <kshiv.dot@gmail.com>
2023-01-19 21:32:47 +0530

    Some updates from previous day in product section

--- OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/Controller/ProductController.java
+++ OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/Controller/ProductController.java
@@ -1,8 +1,73 @@
 package com.OnlineShoppingApp.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ProductController {
+import com.OnlineShoppingApp.Entity.Product;
+import com.OnlineShoppingApp.Service.ProductService;
 
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	
+	@Autowired
+	private ProductService pservice;
+	
+	@PostMapping("/")
+	public ResponseEntity<Product> addProductHandler(@RequestBody Product product){
+		
+		return new ResponseEntity<Product>(pservice.addProduct(product), HttpStatus.CREATED);
+	
+	}
+	
+	
+	@GetMapping("/")
+	public ResponseEntity<List<Product>> viewAllProductHandler(){
+		
+		return new ResponseEntity<List<Product>>(pservice.viewAllProduct(), HttpStatus.OK);
+	
+	}
+	
+	@PutMapping("/")
+	public ResponseEntity<Product> updateProductHandler(@RequestBody Product product){
+		
+		return new ResponseEntity<Product>(pservice.updateProduct(product), HttpStatus.CREATED);
+	
+	}
+	
+	
+	@GetMapping("/{pid}")
+	public ResponseEntity<Product> viewProductByIdHandler(@PathVariable("pid") Integer productId){
+		
+		return new ResponseEntity<Product>(pservice.viewProductById(productId), HttpStatus.OK);
+	
+	}
+	
+	
+	@GetMapping("/{cname}")
+	public ResponseEntity<List<Product>> viewProductByCategoryHandler(@PathVariable("cname") String cname){
+		
+		return new ResponseEntity<List<Product>>(pservice.viewProductByCategory(cname), HttpStatus.OK);
+	
+	}
+	
+	
+	@DeleteMapping("/{pid}")
+	public ResponseEntity<Product> removeProductHandler(@PathVariable("pid") Integer productId){
+		
+		return new ResponseEntity<Product>(pservice.removeProduct(productId), HttpStatus.OK);
+	
+	}
+	
 }
--- OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/Repository/ProductDao.java
+++ OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/Repository/ProductDao.java
@@ -1,11 +1,18 @@
 package com.OnlineShoppingApp.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.OnlineShoppingApp.Entity.Category;
 import com.OnlineShoppingApp.Entity.Product;
 
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer>{
-
+	
+	public Product findByProductName(String productName);
+	
+	public List<Product> findByCategory(Category category);
+	
 }
