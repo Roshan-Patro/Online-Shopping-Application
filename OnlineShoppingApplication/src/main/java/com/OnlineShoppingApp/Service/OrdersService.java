d6f0a4d5774930c222d1c1c99f0fea5f683caebe
Shiv-96 <kshiv.dot@gmail.com>
2023-01-20 17:44:02 +0530

    Some updates in order service section

--- OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/Service/OrdersService.java
+++ OnlineShoppingApplication/src/main/java/com/OnlineShoppingApp/Service/OrdersService.java
@@ -1,5 +1,27 @@
 package com.OnlineShoppingApp.Service;
 
-public interface OrdersService {
+import java.time.LocalDate;
+import java.util.List;
 
+import com.OnlineShoppingApp.Entity.Orders;
+import com.OnlineShoppingApp.Exception.AddressException;
+import com.OnlineShoppingApp.Exception.CustomerException;
+import com.OnlineShoppingApp.Exception.OrdersException;
+
+public interface OrdersService {
+	
+	public Orders addOrder(Orders order, Integer customerID, Integer addressId) throws OrdersException, CustomerException, AddressException;
+	
+	public Orders updateOrder(Orders order) throws OrdersException;
+	
+	public Orders removeOrder(Orders order) throws OrdersException;
+	
+	public List<Orders> getAllOrderByOrderId(Integer orderId) throws OrdersException;
+	
+	public List<Orders> getAllOrder(LocalDate date) throws OrdersException;
+	
+	public List<Orders> getAllOrderByLocation(String loc) throws OrdersException;
+	
+	public List<Orders> getAllOrderByUserId(String userId) throws OrdersException;
+	
 }
